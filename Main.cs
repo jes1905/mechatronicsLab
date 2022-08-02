14007227c594b2ae59a0e570ee5e39de053934b7
jes1905 <jadesanche2005@gmail.com>
2022-08-02 14:24:05 -0400

    worked on json, not finished

--- Main.cs
+++ Main.cs
@@ -5,60 +5,87 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using System.IO;
 using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.JsonSerialization;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Ling;
+using Newtonsoft.Json.Linq;
 
+[Serializable]
 public class Main : MonoBehaviour
 {
 
     public static List<string> data = new List<string>();
     public static string text;
     public TMPro.TMP_Dropdown myDrop;
+    public static string filePath = "data.json";
 
     public void OnClick()
     {
         data.Add("a");
         data.Add("b");
         data.Add("c");
-        data.Add("d");
-        data.Add("e");
         Debug.Log("finished");
         Debug.Log(string.Join("," , data));
         text = string.Join(",", data);
     }
 
+    // makes txt file
     public void MakeTXT()
     {
-        //Debug.Log(string.Join(",", data));
-        //Debug.Log("done");
-
         File.WriteAllTextAsync("dummy.txt" , text);
 
         Debug.Log(text);
-        //string test = "dummy";
 
-        //File.WriteAllTextAsync("dummy2.txt", test );
-       //Debug.Log("complete");
     }
+
 
     //Still working on
-    public void JsonSerialize(object data, string filePath)
+    class DataSerializer
     {
+      //Serializing function
+      public void JsonSerialize(object data, string filePath)
+      {
+          JsonSerializer jsonSerializer = new JsonSerializer();
+          if(File.Exists(filePath)) File.Delete(filePath);
+          StreamWriter sw = new StreamWriter(filePath);
+          JsonWriter jsonWriter = new JsonTextWriter(sw);
+
+          jsonSerializer.Serialize(jsonWriter, data);
+
+          jsonWriter.Close();
+          sw.Close();
+
+      }
+
+      //Deserializes then writes
+      public object JsonDeserialize(Type dataType, string filePath)
+      {
+        JObject obj = null;
         JsonSerializer jsonSerializer = new JsonSerializer();
+        if(File.Exists(filePath))
+        {
+          StreamReader sr = new StreamReader(filePath);
+          JsonReader jsonReader = new JsonTextReader(sr);
+          obj = jsonSerializer.Deserialize(jsonReader) as JObject;
+          sr.Close();
+        }
 
+        return obj.ToObject(dataType);
+      }
     }
 
-    /*public void MakeJSON()
+    public void MakeJSON()
     {
+      DataSerializer dataSerializer = new DataSerializer();
+      dataSerializer.JsonSerialize(text, filePath);
+      dataSerializer.JsonDeserialize(text.GetType(), filePath);
 
-        var json = JsonSerializer.Serialize(data);
-        File.WriteAllText("file.json", json);
+      Debug.Log("fine");
 
-        string output = JsonUtility.ToJson(text);
-
-        Debug.Log(output);
-
-        File.WriteAllTextAsync("file.txt", output);
     }
- */
+
 }
